9156e702c828be92858bd689b5158defac3e1bc6
shubiour <sv95.shuvo@gmail.com>
2024-10-27 21:04:13 +0600

    Sidebar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import NewProject from "./components/NewProject";
+import Sidebar from "./components/Sidebar";
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <Sidebar/>
+      <NewProject />
+    </main>
   );
 }
 
